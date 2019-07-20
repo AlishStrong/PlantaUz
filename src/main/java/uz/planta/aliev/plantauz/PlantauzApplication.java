b3e2c0429e3d3abe21b3c8b6c78c2ea389bce09a
AlishStrong <alishet1990@gmail.com>
2019-07-20 11:53:24 +0300

    Override the project

---
+++ src/main/java/uz/planta/aliev/plantauz/PlantauzApplication.java
@@ -0,0 +1,13 @@
+package uz.planta.aliev.plantauz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantauzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantauzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/planta/aliev/plantauz/PlantauzApplicationTests.java
@@ -0,0 +1,16 @@
+package uz.planta.aliev.plantauz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlantauzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
